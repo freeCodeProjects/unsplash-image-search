51dc3bbff9ce2a93c162cd53f01cc2fc7379cbc9
Akash187 <kumarakash007@gmail.com>
2021-07-20 20:02:40 +0530

    feat: implemented search feature

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //@ts-check
 const clientId = 'VK-_5v_JjwISU3_wspDxJHk84YuVUJLWO-ERrtUH_0I'
-let perPage, page, images, fetchMore
+let perPage, page, images, fetchMore, searchText, previousSearchText
 
 const fetchBackgroundImage = async () => {
 	const response = await fetch(
@@ -13,20 +13,42 @@ const fetchBackgroundImage = async () => {
 	heroELm.style.backgroundSize = 'cover'
 }
 
-const fetchImages = async () => {
+const fetchQueryImages = async () => {
+	const response = await fetch(
+		`https://api.unsplash.com/search/photos/?client_id=${clientId}&query=${searchText}&per_page=${perPage}&page=${page}`
+	)
+	const res = await response.json()
+
+	//render fetched images
+	renderImages(res.results)
+}
+
+const fetchInitialImages = async () => {
 	console.log('feching images')
 
 	const response = await fetch(
 		`https://api.unsplash.com/photos/?client_id=${clientId}&per_page=${perPage}&page=${page}`
 	)
-	const imgs = await response.json()
-	const tempImages = {}
+	const res = await response.json()
+
+	//render fetched images
+	renderImages(res)
+}
+
+const renderImages = (imgs) => {
+	//hide loader when response is empty
+	if (imgs.length === 0) {
+		document.querySelector('.loader').classList.add('hide')
+	}
+
+	const currImages = {}
 
 	imgs.forEach((img) => {
-		tempImages[img.id] = {
+		currImages[img.id] = {
 			id: img.id,
 			height: img.height,
 			width: img.width,
+			smallImageUrl: img.urls.small,
 			regularImageUrl: img.urls.regular,
 			fullImageUrl: img.urls.full,
 			user: img.user.name,
@@ -35,25 +57,13 @@ const fetchImages = async () => {
 		}
 	})
 
-	//render fetched images
-	renderImages(tempImages)
-
-	//update page number
-	page += 1
-
-	images = { ...images, ...tempImages }
-	console.log(images)
-}
-
-const renderImages = (imgs) => {
 	const misonaryElm = document.querySelector('.misonary')
-
 	let imagesHTML = ''
-	for (let id in imgs) {
-		const img = imgs[id]
+	for (let id in currImages) {
+		const img = currImages[id]
 
 		imagesHTML += `<figure>
-			<img src="${img.regularImageUrl}" alt="${img.description}" data-id="${img.id}" loading="lazy"/>
+			<img src="${img.smallImageUrl}" alt="${img.description}" data-id="${img.id}" loading="lazy"/>
 			<figcaption>
 				<div class="user">
 					<img
@@ -71,6 +81,12 @@ const renderImages = (imgs) => {
 	//append recently fetched images at the end of misonary element
 	misonaryElm.insertAdjacentHTML('beforeend', imagesHTML)
 
+	//update page number
+	page += 1
+
+	//update images object
+	images = { ...images, ...currImages }
+
 	//reset fetchMore variable
 	fetchMore = true
 }
@@ -85,7 +101,7 @@ const fetchInfiniteImages = () => {
 	let observer = new IntersectionObserver((entries) => {
 		if (entries[0].isIntersecting) {
 			console.log('intersecting')
-			fetchMore && fetchImages()
+			fetchMore && (searchText ? fetchQueryImages() : fetchInitialImages())
 			fetchMore = false
 		} else {
 			console.log('not Intersecting')
@@ -96,11 +112,45 @@ const fetchInfiniteImages = () => {
 	observer.observe(target)
 }
 
+const search = () => {
+	if (previousSearchText === searchText) return
+	else previousSearchText = searchText
+
+	//after resetting innerHTML of misonary Element loader will come in view and it will auto trigger fetching of images.
+	const misonaryElm = document.querySelector('.misonary')
+	misonaryElm.innerHTML = ''
+
+	//reset other variable
+	images = {}
+	page = 1
+	document.querySelector('.loader').classList.remove('hide')
+}
+
+const searchInputEventListener = () => {
+	const inputElm = document.querySelector('.searchInput')
+	inputElm.addEventListener('keydown', (e) => {
+		const newValue = e.target.value.trim()
+		searchText = e.target.value
+	})
+}
+
+const formSubmitEventListener = () => {
+	const form = document.querySelector('form')
+	form.addEventListener('submit', (e) => {
+		e.preventDefault()
+		search()
+	})
+}
+
 ;(async function () {
 	perPage = 12
 	page = 1
 	images = {}
 	fetchMore = true
+	searchText = ''
+	previousSearchText = ''
+	searchInputEventListener()
+	formSubmitEventListener()
 	fetchInfiniteImages()
 	await fetchBackgroundImage()
 })()
